cc2c4fdbd35f41f9f7ef8c1d2996e504a9865b99
Jack Sheridan <jackrileysheridan@gmail.com>
2022-03-13 16:54:34 -0700

    added warning

--- assets/ios-beta.js
+++ assets/ios-beta.js
@@ -1,6 +1,8 @@
 window.location.href = "itms-beta://testflight.apple.com/join/BQ3XlrGX";
 
+const header = document.getElementById("header");
 setInterval(() => {
-console.log("I waited!")
+header.innerText = "Waited 3 seconds. Maybe you don't have the TestFlight app?";
+console.log("I waited!");
 }, 3000);
 
