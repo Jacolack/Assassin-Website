7c6b5b143499ccf6934ba2d7f727085b03cc13ac
Jack Sheridan <jackrileysheridan@gmail.com>
2022-03-13 17:10:34 -0700

    timeout

--- assets/ios-beta.js
+++ assets/ios-beta.js
@@ -1,7 +1,7 @@
 window.location.href = "itms-beta://testflight.apple.com/join/BQ3XlrGX";
 
 const header = document.getElementById("header");
-setInterval(() => {
+setTimeout(() => {
 header.innerText = "Waited 3 seconds. Maybe you don't have the TestFlight app?";
 console.log("I waited!");
 }, 3000);
